d0e49255afd897a79046d9d81c79277140c5db66
miysono <stefanghidrigan@gmail.com>
2023-09-30 23:13:57 +0300

    added expand layout

--- src/components/application/layout/LayoutBody.jsx
+++ src/components/application/layout/LayoutBody.jsx
@@ -1,14 +1,22 @@
 import { useLayoutContext } from "../../../hooks/useLayoutContext";
 
 export default function LayoutBody({ children }) {
-  const { isLayoutOpen } = useLayoutContext();
+  const { isLayoutOpen, setIsLayoutOpen } = useLayoutContext();
   return (
-    <div
-      className={`z-50 absolute w-1/3 max-h-full duration-500 h-screen text-blue-400 bg-gray-800 overflow-y-auto overflow-x-hidden ${
-        isLayoutOpen ? "" : "-translate-x-full"
-      }`}
-    >
-      {children}
-    </div>
+    <>
+      <div
+        className="left-0 top-1/2 -translate-y-1/2 absolute z-50 text-gray-800"
+        onClick={() => setIsLayoutOpen(true)}
+      >
+        <button className="text-6xl">&gt;</button>
+      </div>
+      <div
+        className={`z-50 absolute w-1/3 max-h-full duration-500 h-screen text-blue-400 bg-gray-800 overflow-y-auto overflow-x-hidden ${
+          isLayoutOpen ? "" : "-translate-x-full"
+        }`}
+      >
+        {children}
+      </div>
+    </>
   );
 }
--- src/components/application/layout/LayoutHeader.jsx
+++ src/components/application/layout/LayoutHeader.jsx
@@ -4,7 +4,7 @@ export default function LayoutHeader() {
   return (
     <Link to="/">
       <div className="py-5 text-blue-400 flex justify-center items-center">
-        <img src="logo-icon-blue.png" className="w-20" />
+        <img src="/logo-icon-blue.png" className="w-12" />
         <p className="text-4xl tracking-tight font-semibold">Cloudtinum</p>
       </div>
     </Link>
